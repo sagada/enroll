e9b84c104c3ecd881325bb7082b62b97e999ef69
sagada <wlatmdqja@naver.com>
2022-11-22 01:17:38 +0900

    수정

--- src/test/java/com/sugang/toys/command/enroll/domain/EnrollmentRepositoryTest.java
+++ src/test/java/com/sugang/toys/command/enroll/domain/EnrollmentRepositoryTest.java
@@ -1,17 +1,21 @@
 package com.sugang.toys.command.enroll.domain;
 
-import com.sugang.toys.command.course.domain.CourseRepository;
-import com.sugang.toys.command.course.domain.CreateCourseValidator;
+import com.sugang.toys.command.course.domain.*;
 import com.sugang.toys.command.department.domain.Department;
 import com.sugang.toys.command.enroll.application.EnrollmentCreateValidate;
 import com.sugang.toys.command.professor.domain.Professor;
+import com.sugang.toys.command.student.domain.Student;
 import com.sugang.toys.command.student.domain.StudentRepository;
 import com.sugang.toys.config.JpaRepositoryTestConfiguration;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class EnrollmentRepositoryTest extends JpaRepositoryTestConfiguration {
 
@@ -36,42 +40,42 @@ public class EnrollmentRepositoryTest extends JpaRepositoryTestConfiguration {
     @Mock
     EnrollmentCreateValidate enrollmentCreateValidate;
 
-//    @Test
-//    void createTest()
-//    {
-//        Student student = new Student("name");
-//        Course course = Course.createCourse(
-//                Set.of(new CourseSchedule(DayOfWeek.FRIDAY, LocalDateTime.now(), LocalDateTime.now(), "1004"))
-//                , Set.of(new CourseSummary(1, "content", "title"))
-//                , professor
-//                ,"courseName"
-//                , department
-//                , "bookName"
-//                , 1
-//                , createCourseValidator
-//        );
-//
-//        studentRepository.save(student);
-//        courseRepository.save(course);
-//
-//        Enrollment enrollment = Enrollment.enroll(
-//                course
-//                , student
-//                , enrollmentCreateValidate
-//        );
-//
-//        Enrollment save = enrollmentRepository.save(enrollment);
-//
-//        Assertions.assertThat(save).isNotNull();
-//        Assertions.assertThat(save.getEnrolmentStatus()).isEqualTo(EnrolmentStatus.PROPOSE);
-//        Assertions.assertThat(save.getCourseId()).isNotNull();
-//        Assertions.assertThat(save.getEnrollStudent()).isNotNull();
-//    }
+    @Test
+    void createTest()
+    {
+        Student student = new Student("name");
+        Course course = Course.createCourse(
+                Set.of(new CourseSchedule(DayOfWeek.FRIDAY, LocalDateTime.now(), LocalDateTime.now(), "1004"))
+                , Set.of(new CourseSummary(1, "content", "title"))
+                , professor
+                ,"courseName"
+                , department
+                , "bookName"
+                , 1
+                , createCourseValidator
+        );
+
+        studentRepository.save(student);
+        courseRepository.save(course);
+
+        Enrollment enrollment = Enrollment.enroll(
+                course.getId()
+                , student.getId()
+                , enrollmentCreateValidate
+        );
+
+        Enrollment save = enrollmentRepository.save(enrollment);
+
+        Assertions.assertThat(save).isNotNull();
+        Assertions.assertThat(save.getEnrolmentStatus()).isEqualTo(EnrolmentStatus.PROPOSE);
+        Assertions.assertThat(save.getCourseId()).isNotNull();
+        Assertions.assertThat(save.getEnrollStudent()).isNotNull();
+    }
 
     @Test
     void enrollFindTest()
     {
         List<Long> enrollmentListByStudentId = enrollmentRepository.findEnrollmentListByStudentId(1L);
-        System.out.println(enrollmentListByStudentId);
+        System.out.println("fined :" + enrollmentListByStudentId);
     }
 }
