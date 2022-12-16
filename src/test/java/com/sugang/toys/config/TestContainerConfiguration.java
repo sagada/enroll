52c938c24f5bd5ffc793ee7a0a2e6226d06d0c96
saga <wlatmdqja@naver.com>
2022-12-17 01:27:40 +0900

    도커 컴포즈 테스트 Config 수정

--- src/test/java/com/sugang/toys/config/TestContainerConfiguration.java
+++ src/test/java/com/sugang/toys/config/TestContainerConfiguration.java
@@ -1,15 +1,19 @@
 package com.sugang.toys.config;
 
 import org.junit.ClassRule;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.File;
 
+@SpringBootTest
 @Transactional
-@Import(value = TestCustomConfiguration.class)
+@ContextConfiguration(classes = TestCustomConfiguration.class)
+@TestPropertySource(locations = "classpath:application-integration.yml")
 @Testcontainers
 public class TestContainerConfiguration{
 
